8a60407731e9fdfb4de8033909f42607905f55d0
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-04-18 02:27:41 -0300

    added url, techs and fixes tests

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -15,20 +15,20 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, owner } = request.body;
-  const newRepo = { id: uuid(), title, owner, likes: 0 };
+  const { title, url, techs } = request.body;
+  const newRepo = { id: uuid(), title, url, techs, likes: 0 };
   repositories.push(newRepo);
   return response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title, owner } = request.body;
+  const { title, url, techs } = request.body;
   const repoIndex = repositories.findIndex(repo => repo.id === id);
   if (repoIndex < 0) return response.status(400).json({error: 'Project not found'})
-  const newRepo = {id, title, owner};
+  const newRepo = {id, title, url, techs};
   repositories[repoIndex] = {...repositories[repoIndex], ...newRepo};
-  return response.json(newRepo);
+  return response.json(repositories[repoIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -45,7 +45,7 @@ app.post("/repositories/:id/like", (request, response) => {
   if (repoIndex < 0) return response.status(400).json({error: 'Project not found'})
   const { likes } = repositories[repoIndex]
   repositories[repoIndex].likes = likes + 1
-  return response.status(204).send();
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
